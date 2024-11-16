8812fb8f4ba9009e6be6ab77dcd43bd274004e6f
Akash <akash.puranik06@gmail.com>
2024-11-16 15:50:51 +0530

    routes and redirects

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
     console.log("in the middleware");
-    next();
-});
+    res.send(`<form action='/product' method='POST'>title<input type='text' name='title'></inpt>size<input type='text' name='size'></inpt><button type='submit'>add</button>`);
 
-app.use((req,res,next)=>{
-    res.send(`<h1>Hello there</h1>`);
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    res.send(`<h1>home page</h1>`);
 });
 
 app.listen(4000);
